26e0c4462d12581f7ff5b31038114439d1d625eb
RochauD <rochau.dev@gmail.com>
2014-10-04 23:17:39 +0200

    Added close event.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,10 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+			{
+				window.close();
+			}
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
