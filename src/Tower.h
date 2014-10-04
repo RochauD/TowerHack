6295f834373701aebf78a66405fd4158b24c3aaf
Robert Williger <ufebg@student.kit.edu>
2014-10-05 01:20:46 +0200

    Added Tower class.

---
+++ src/Tower.h
@@ -0,0 +1,14 @@
+#ifndef TOWER_H
+#define TOWER_H
+#include "Entity.h"
+#include "SFML\System.hpp"
+
+class Tower : public Entity {
+private:
+	//projectile
+	sf::Time projectileSpeed;
+
+};
+
+
+#endif
