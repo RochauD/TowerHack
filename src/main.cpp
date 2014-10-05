dbf4901fee758498973a6ea479ac1644b9039db5
RochauD <rochau.dev@gmail.com>
2014-10-05 07:04:12 +0200

    Changed Projekt files.

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,15 @@
 #include "AnimationSprite.h"
 #include "RectTexture.h"
 
+
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1000, 900), "SFML works!");
 	window.setVerticalSyncEnabled(true);
 	RenderManager renderManger("", &window);
 
-	std::string str = "bild1.png";
-	std::string str2 = "bild2.png";
-	StaticSprite<StandardTexture> test(str, 100);
-	StaticSprite<StandardTexture> test2(str, 10);
-	AnimationSprite<RectTexture> test3("info.txt", 1000, true, sf::milliseconds(10000 / 24), false, true);
-	test.setPosition(0, 0);
-	test2.setPosition(100, 200);
+	
 
 
 	while (window.isOpen())
